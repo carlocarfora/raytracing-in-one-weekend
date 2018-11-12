f33f287790c621dbc96085b9e386cf4ddd84b5bd
carloc <carlo@carlocarfora.co.uk>
2018-11-12 22:10:07 +0000

    restructure and use clion

---
+++ src/vec3.cpp
@@ -0,0 +1,5 @@
+//
+// Created by carlo on 11/11/18.
+//
+
+#include "vec3.hpp"
---
+++ src/vec3.hpp
@@ -0,0 +1,15 @@
+//
+// Created by carlo on 11/11/18.
+//
+
+#ifndef RAYTRACING_IN_ONE_WEEKEND_VEC3_HPP
+#define RAYTRACING_IN_ONE_WEEKEND_VEC3_HPP
+
+#include <math.h>
+
+struct vec3 {
+
+};
+
+
+#endif //RAYTRACING_IN_ONE_WEEKEND_VEC3_HPP
